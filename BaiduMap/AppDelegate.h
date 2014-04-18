695049f4f7aa95eb61a2f4186bbf79ffb3ffcff4
qianfeng <apple@aofel.local>
2014-04-18 15:19:55 +0800

    Initial Commit

---
+++ BaiduMap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BaiduMap
+//
+//  Created by qianfeng on 14-4-18.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
